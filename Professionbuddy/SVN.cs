432c6975eb6cf8b89f0bf17c44c1ad5a60c42d1b
HighVoltz82 <HighVoltz82@gmail.com>
2011-09-17 01:28:50 +0000

    GetMail Action bug fix for retrieval of specific items

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2011/09/11 14:39:39
+// Build Date: 2011/09/12 13:43:28
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 using System;
@@ -16,7 +16,7 @@ namespace HighVoltz
         {
             get
             {
-                return "124";
+                return "127";
             }
         }
     }
