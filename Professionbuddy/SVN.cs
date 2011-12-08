f0a393f1ddd2e1e7598cc8aa31f3e2d2dafa2443
HighVoltz82 <HighVoltz82@gmail.com>
2011-12-08 05:39:43 +0000

    new* 'SwitchCharacter(string character,string server) – Logs out and switches to character on specified server, call this from a custom action

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2011/12/01 19:36:26
+// Build Date: 2011/12/08 00:31:53
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 using System;
@@ -16,7 +16,7 @@ namespace HighVoltz
         {
             get
             {
-                return "148";
+                return "149";
             }
         }
     }
