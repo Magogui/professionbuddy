684517ddff0f0b9e1d887b54cc8c1373178e1592
HighVoltz82 <HighVoltz82@gmail.com>
2012-01-09 16:13:04 +0000

    Mining-herb 1-535 bugfix

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/01/08 17:05:08
+// Build Date: 2012/01/09 01:38:47
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 using System;
@@ -16,7 +16,7 @@ namespace HighVoltz
         {
             get
             {
-                return "262";
+                return "264";
             }
         }
     }
