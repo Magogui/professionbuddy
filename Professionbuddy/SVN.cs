1b44c793e90fb6d4e56f930a697bad6ca9ccf844
HighVoltz82 <HighVoltz82@gmail.com>
2012-01-28 18:16:37 +0000

    Multiprofile.xml small tweak

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/01/28 11:37:37
+// Build Date: 2012/01/28 12:54:25
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 using System;
@@ -16,7 +16,7 @@ namespace HighVoltz
         {
             get
             {
-                return "344";
+                return "345";
             }
         }
     }
