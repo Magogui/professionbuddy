c1094560261cf3f16fd80866c5f9276d244d2d54
HighVoltz82 <HighVoltz82@gmail.com>
2012-04-14 02:06:04 +0000

    Attempt at fixing training.

--- Professionbuddy/Composites/TrainSkillAction.cs
+++ Professionbuddy/Composites/TrainSkillAction.cs
@@ -133,7 +133,8 @@ namespace HighVoltz.Composites
                 {
                     _waitBeforeTrainingStopWatch.Reset();
                     _concludingStopWatch.Reset();
-                    Lua.DoString("BuyTrainerService(0) ");
+                    //Lua.DoString("BuyTrainerService(0) ");
+                    Lua.DoString("for i=GetNumTrainerServices(),1,-1 do if select(3,GetTrainerServiceInfo(2)) == 'available' then BuyTrainerService(i) end end");
                     Professionbuddy.Log("Training Completed ");
                     IsDone = true;
                 }
