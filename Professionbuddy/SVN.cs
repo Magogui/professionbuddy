af6d8c8374fe8e73dc4d9afa8078a58177ce7bd5
HighVoltz82 <HighVoltz82@gmail.com>
2012-04-19 16:58:53 +0000

    Some properties were not being displayed, this is fixed.

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/04/15 15:22:58
+// Build Date: 2012/04/15 16:39:24
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 using System;
@@ -16,7 +16,7 @@ namespace HighVoltz
         {
             get
             {
-                return "459";
+                return "460";
             }
         }
     }
