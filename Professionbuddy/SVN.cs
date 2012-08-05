d4d82526369b27f0f34242afb1a834fc2ad583b6
HighVoltz82 <HighVoltz82@gmail.com>
2012-08-05 23:44:11 +0000

    Multiprofile should no longer get stuck in Twilight Highlands

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/07/29 10:39:37
+// Build Date: 2012/07/29 19:04:55
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 using System;
@@ -16,7 +16,7 @@ namespace HighVoltz
         {
             get
             {
-                return "472";
+                return "473";
             }
         }
     }
