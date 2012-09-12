ceb168fc6f6aa206fe36fccc85794a12e066c8e6
HighVoltz82 <HighVoltz82@gmail.com>
2012-09-12 15:45:42 +0000

    Fixed MultiProfile

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/09/11 14:49:16
+// Build Date: 2012/09/11 14:58:52
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "486";
+                return "487";
             }
         }
     }
