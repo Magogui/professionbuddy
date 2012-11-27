b0116b25028f78146f058be2d655478e8b2b3f5e
HighVoltz82 <HighVoltz82@gmail.com>
2012-11-27 05:34:22 +0000

    [Fixed] adjusted the waypoints in KunLai Summit profile to avoid a certain annoying elite.

--- Professionbuddy/Dynamic/DynamicCodeCompiler.cs
+++ Professionbuddy/Dynamic/DynamicCodeCompiler.cs
@@ -147,7 +147,6 @@ namespace HighVoltz.Dynamic
                 var options = new CompilerParameters();
                 foreach (Assembly asm in AppDomain.CurrentDomain.GetAssemblies())
                 {
-                    asm.GetTypes();
                     if (!asm.GetName().Name.Contains(Professionbuddy.Instance.Name) && !asm.IsDynamic)
                         options.ReferencedAssemblies.Add(asm.Location);
                 }
--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/11/24 20:29:41
+// Build Date: 2012/11/26 20:58:02
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "525";
+                return "526";
             }
         }
     }
