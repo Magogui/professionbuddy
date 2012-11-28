f2b97107a542696bebc8b26ab6b9048147a9a537
HighVoltz82 <HighVoltz82@gmail.com>
2012-11-28 18:42:22 +0000

    [Changed] Mining-Herb (1-600) profile nolonger ignores gray nodes as the feature isn't fully functional

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/11/26 20:58:02
+// Build Date: 2012/11/26 21:34:22
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "526";
+                return "527";
             }
         }
     }
