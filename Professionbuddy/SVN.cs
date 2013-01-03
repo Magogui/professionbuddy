df4e47447514f8ac5b5863454ae0489fc892847e
HighVoltz82 <HighVoltz82@gmail.com>
2013-01-03 15:29:33 +0000

    [Fixed] Ignore grey herbs in Mining-Herb (1-600)flying-GB2.xml should now work.

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2012/12/25 08:37:55
+// Build Date: 2013/01/02 16:28:07
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "538";
+                return "539";
             }
         }
     }
