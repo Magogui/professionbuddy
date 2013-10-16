7fc28bd912812767482fba2eb563de3357815281
HighVoltz82 <HighVoltz82@gmail.com>
2013-10-16 17:00:53 +0000

    'Item Entry' and 'Item Entries' string are now changed to show 'Item ID' and 'Item IDs'

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2013/10/15 23:05:56
+// Build Date: 2013/10/16 12:19:12
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "614";
+                return "615";
             }
         }
     }
