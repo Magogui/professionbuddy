b64b46e08a5672b29b15d1b4b461429a27aea59e
HighVoltz82 <HighVoltz82@gmail.com>
2014-04-08 00:53:49 +0000

    Fixed a training loop in the Pandara mining/herb leveling profile
    Added a blackspot around Camp Nooka Nooka and Pearlfin Village in the Jade forest neutral herb/mining profile.

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/03/18 10:09:41
+// Build Date: 2014/03/26 18:25:35
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "650";
+                return "652";
             }
         }
     }
