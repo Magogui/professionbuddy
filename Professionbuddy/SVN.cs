c04cd344e0c8e22cfcc7d1e3e12783a26b809c42
HighVoltz82 <HighVoltz82@gmail.com>
2014-05-05 20:16:35 +0000

    Deleted a few unused files.

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/04/16 23:55:18
+// Build Date: 2014/05/03 20:13:10
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "654";
+                return "655";
             }
         }
     }
