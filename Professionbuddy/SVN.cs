142059dd4d4b9f0cb028ba1f96682c63956b0960
HighVoltz82 <HighVoltz82@gmail.com>
2014-06-12 03:50:49 +0000

    AHBot.xml will now list Golden Lotus on AH for higher prices

--- Professionbuddy/SVN.cs
+++ Professionbuddy/SVN.cs
@@ -1,5 +1,5 @@
 ﻿// This file is auto genorated from using SubRCRev.exe and template SVN.template
-// Build Date: 2014/06/05 14:59:46
+// Build Date: 2014/06/11 15:21:51
 // SVN url: https://professionbuddy.googlecode.com/svn/trunk/Professionbuddy
 
 namespace HighVoltz
@@ -10,7 +10,7 @@ namespace HighVoltz
         {
             get
             {
-                return "669";
+                return "671";
             }
         }
     }
